8402e4272e4770032f13324dafc805c6bd07f75e
Tobia-Claut <tobiaclaut03@gmail.com>
2025-05-08 19:43:16 +0200

    creati test per i primi 3 numeri close #8

--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -2,3 +2,24 @@
 // [STUDENT-1-NAME-SURNAME] [STUDENT-1-ID]
 // [STUDENT-2-NAME-SURNAME] [STUDENT-2-ID]
 ////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class IntegerToRomanTest {
+    @Test
+    public void testConvert4() {
+        assertEquals("I", IntegerToRoman.convert(1));
+    }
+
+    @Test
+    public void testConvert5() {
+        assertEquals("II", IntegerToRoman.convert(2));
+    }
+
+    @Test
+    public void testConvert6() {
+        assertEquals("III", IntegerToRoman.convert(3));
+    }
+}
--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -2,3 +2,48 @@
 // [STUDENT-1-NAME-SURNAME] [STUDENT-1-ID]
 // [STUDENT-2-NAME-SURNAME] [STUDENT-2-ID]
 ////////////////////////////////////////////////////////////////////
+
+
+package it.unipd.mtss;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class RomanPrinterTest {
+
+    @Test
+    public void testPrint1to3() {
+        // Test per il numero 1 (I)
+        assertEquals(
+            "  _____   \n" +
+            " |_   _|  \n" +
+            "   | |    \n" +
+            "   | |    \n" +
+            "  _| |_   \n" +
+            " |_____|  \n",
+            RomanPrinter.print(1)
+        );
+
+        // Test per il numero 2 (II)
+        assertEquals(
+            "  _____     _____   \n" +
+            " |_   _|   |_   _|  \n" +
+            "   | |       | |    \n" +
+            "   | |       | |    \n" +
+            "  _| |_     _| |_   \n" +
+            " |_____|   |_____|  \n",
+            RomanPrinter.print(2)
+        );
+
+        // Test per il numero 3 (III)
+        assertEquals(
+            "  _____     _____     _____   \n" +
+            " |_   _|   |_   _|   |_   _|  \n" +
+            "   | |       | |       | |    \n" +
+            "   | |       | |       | |    \n" +
+            "  _| |_     _| |_     _| |_   \n" +
+            " |_____|   |_____|   |_____|  \n",
+            RomanPrinter.print(3)
+        );
+    }
+}
